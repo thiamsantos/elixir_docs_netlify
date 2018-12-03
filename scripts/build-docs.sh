b21d38002c9ab4ac8d59d90159febc1f7d3dbcd3
Thiago Santos <thia.mdossantos@gmail.com>
2018-12-03 17:30:04 -0200

    wip

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -10,10 +10,12 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.6.2
 asdf plugin-add erlang
 asdf plugin-add elixir
 
-asdf plugin-install erlang 20.3
-asdf plugin-install elixir 1.6.6
+asdf install erlang 20.3
+asdf install elixir 1.6.6
 
 asdf global erlang 20.3
 asdf global elixir 1.6.6
 
+mix local.hex --force
+mix deps.get
 mix docs
