f408e33d2228306805b09eb8ef0c92612d34a574
Thiago Santos <thia.mdossantos@gmail.com>
2018-12-03 17:24:47 -0200

    wip

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-mkdir doc
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.6.2
 
-echo "$(pwd)" > doc/index.html
+export HOME="$HOME/.asdf/asdf.sh"
+export HOME="$HOME/.asdf/completions/asdf.bash"
+
+asdf plugin-add erlang
+asdf plugin-add elixir
+
+asdf plugin-install erlang 20.3
+asdf plugin-install elixir 1.6.6
+
+asdf global erlang 20.3
+asdf global elixir 1.6.6
+
+mix docs
